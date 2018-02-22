f9e17948e19ddffb6d02d36548811fe016813d07
Skylar Onstot <skylar@themousepotato.net>
2018-02-22 01:15:45 -0800

    Create EndianSwap.java

---
+++ Bowtie2Java/com/uwb/bt2j/util/EndianSwap.java
@@ -0,0 +1,16 @@
+class EndianSwap {
+  public static double endianSwapU32 (double x) {
+    
+  }
+  public static int endianSwapI32 (double x) {
+    
+  }
+  
+  public static double endianSwapI64 (double x) {
+    
+  }
+  
+  public static long endianSwapU64 (long x) {
+    
+  }
+}
