33b707461251c683905882c8747bdc2d4f05716f
Skylar Onstot <skylar@themousepotato.net>
2018-02-21 16:46:02 -0800

    Update DynamicProgrammingFramer.java

--- Bowtie2Java/com/uwb/bt2j/aligner/dp/DynamicProgrammingFramer.java
+++ Bowtie2Java/com/uwb/bt2j/aligner/dp/DynamicProgrammingFramer.java
@@ -1,1 +1,39 @@
-
+class DynamicProgrammingFramer {
+  public Boolean frameSeedExtensionRect() {
+    
+  }
+  
+  public Boolean frameFindMateAnchorLeftRect() {
+    
+  }
+  
+  public Boolean frameFindMateAnchorRightRect() {
+    
+  }
+  
+  public static void testCaseFindMateAnchorLeft() {
+    
+  }
+  
+  public static void testCaseFindMateAnchorRight() {
+    
+  }
+  
+  public int main() {
+    testCaseFindMateAnchorLeft();
+    testCaseFindMateAnchorLeft();
+    testCaseFindMateAnchorLeft();
+    testCaseFindMateAnchorLeft();
+    testCaseFindMateAnchorLeft();
+    testCaseFindMateAnchorLeft();
+    
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+    testCaseFindMateAnchorRight();
+  }
+}
