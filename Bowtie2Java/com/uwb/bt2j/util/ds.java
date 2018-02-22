6b696b6829b97fe6b80665c9d4d1a46187c89d24
Skylar Onstot <skylar@themousepotato.net>
2018-02-21 16:12:33 -0800

    Update ds.java

--- Bowtie2Java/com/uwb/bt2j/util/ds.java
+++ Bowtie2Java/com/uwb/bt2j/util/ds.java
@@ -1,1 +1,33 @@
-
+class MemoryTally {
+  public MemoryTally() {
+    
+  }
+  
+  public void add() {
+     
+  }
+  
+  public void del() {
+    
+  }
+  
+  public void main() {
+  
+  }
+  
+  public long total() {
+    
+  }
+  
+  public long total(c) {
+    
+  }
+  
+  public long peak() {
+    
+  }
+  
+  public long peak(c) {
+    
+  }
+}
