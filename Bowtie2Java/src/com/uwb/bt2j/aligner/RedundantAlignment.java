1204f82e86e5fb62008629ae2227213bfcca81a6
Skylar Onstot <skylar@themousepotato.net>
2018-02-28 17:48:51 -0800

    Update RedundantAlignment.java

--- Bowtie2Java/src/com/uwb/bt2j/aligner/RedundantAlignment.java
+++ Bowtie2Java/src/com/uwb/bt2j/aligner/RedundantAlignment.java
@@ -43,10 +43,97 @@ public class RedundantAlignment {
   }
   
   public void add(AlignmentResult res) {
-  
+  long left, right = res.refoff();
+	double len = res.readExtentRows();
+	double alignmentStart = res.trimmedLeft(true);
+	if(!res.fw()) {
+		const_cast<AlnRes&>(res).invertEdits();
+	}
+	EList<Edit> ned = res.ned();
+	double nedidx = 0;
+	// For each row...
+	for(double i = alignmentStart; i < alignmentStart + len; i++) {
+		double diff = 1;  // amount to shift to right for next round
+		right = left + 1;
+		while(nedidx < ned.size() && ned[nedidx].pos == i) {
+			if(ned[nedidx].isRefGap()) {
+				// Next my_left will be in same column as this round
+				diff = 0;
+			}
+			nedidx++;
+		}
+		if(i < alignmentStart + len - 1) {
+			// See how many inserts there are before the next read
+			// character
+			double nedidx_next = nedidx;
+			while(nedidx_next < ned.size() && ned[nedidx_next].pos == i+1)
+			{
+				if(ned[nedidx_next].isReadGap()) {
+					right++;
+				}
+				nedidx_next++;
+			}
+		}
+		for(long j = left; j < right; j++) {
+			// Add to db
+			RedundantCell c(res.refid(), res.fw(), j, i);
+		}
+		left = right + diff - 1;
+	}
+	if(!res.fw()) {
+		const_cast<AlnRes&>(res).invertEdits();
+	}
   }
   
   public Boolean overlap(AlignmentResult res) {
-  
+  long left = res.refoff(), right;
+	double len = res.readExtentRows();
+	double alignmentStart = res.trimmedLeft(true);
+	if(!res.fw()) {
+		const_cast<AlnRes&>(res).invertEdits();
+	}
+	EList<Edit> ned = res.ned();
+	double nedidx = 0;
+	// For each row...
+	Boolean olap = false;
+	for(double i = alignmentStart; i < alignmentStart + len; i++) {
+		double diff = 1;  // amount to shift to right for next round
+		right = left + 1;
+		while(nedidx < ned.size() && ned[nedidx].pos == i) {
+			if(ned[nedidx].isRefGap()) {
+				// Next my_left will be in same column as this round
+				diff = 0;
+			}
+			nedidx++;
+		}
+		if(i < alignmentStart + len - 1) {
+			// See how many inserts there are before the next read
+			// character
+			double nedidx_next = nedidx;
+			while(nedidx_next < ned.size() && ned[nedidx_next].pos == i+1)
+			{
+				if(ned[nedidx_next].isReadGap()) {
+					right++;
+				}
+				nedidx_next++;
+			}
+		}
+		for(long j = left; j < right; j++) {
+			// Add to db
+			RedundantCell c(res.refid(), res.fw(), j, i);
+			if(cells_[i].contains(c)) {
+				olap = true;
+				break;
+			}
+		}
+		if(olap) {
+			break;
+		}
+		left = right + diff - 1;
+	}
+	if(!res.fw()) {
+		const_cast<AlnRes&>(res).invertEdits();
+	}
+	return olap;
   }
 }
